0db990f077c2c8a5e26fad15b7c3d7fc3ff97658
Hirschmann <yann.hirschmann@hotmail.com>
2019-02-19 15:36:18 +0100

    Create MainController Create first template

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 /*
- * Welcome to your app's main JavaScript file!
+ * Welcome to your app's pages JavaScript file!
  *
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
@@ -7,8 +7,48 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
+//require('bootstrap');
+
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
+/*const feather = require('feather-icons');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+(function () {
+    'use strict';
+    feather.replace();
+}());*/
+
+$(document).ready(function () {
+    var i = 1;
+
+    $('#sidebar').toggleClass('nav-active');
+
+    $('#btn-vNav').on('click', function () {
+        $('#sidebar').toggleClass('nav-active');
+        $('#content').toggleClass('content-active');
+    });
+
+    $("#btn-add-article").click(function () {
+        window.location.replace("/ajouter");
+    });
+
+    $('#btnfile'+i).on('change',function(){
+        i = addNewInputFile('#btnfile'+i, i)
+    });
+});
+
+function addNewInputFile(btnfileId, i) {
+    $('#feather'+i).html('<span id="feather'+i+'" data-feather="save"></span>');
+    i+= 1;
+    $('.btn-add-files').append('<!----><label class="btn btn-outline-primary Up-file">' +
+        '<input id="btnfile'+i+'" type="file" hidden>' +
+        '<span id="feather'+i+'" data-feather="plus"></span></label>'
+    );
+    $('#btnfile'+i).on('change',function(){
+        i = addNewInputFile('#btnfile'+i, i)
+    });
+    feather.replace();
+    console.log(i);
+    return i;
+}
---
+++ src/Controller/MainController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="_index")
+     * @param Environment $environment
+     * @return Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function indexAction(Environment $environment){
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        return new Response($environment->render('pages/index.html.twig'));
+    }
+}
