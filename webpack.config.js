129b71d4f77e11fdc9056018f01d235fe22623fb
Hirschmann <yann.hirschmann@hotmail.com>
2019-02-20 10:07:31 +0100

    Add vichUploader

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,6 @@ Encore
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
-
     // enables Sass/SCSS support
     //.enableSassLoader()
 
